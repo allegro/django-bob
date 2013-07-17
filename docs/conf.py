225658d83636c18a751bb3dbb27de31c921a2dbe
Radomir Dopieralski <radomir.dopieralski@allegro.pl>
2013-07-17 13:08:21 +0200

    bump docs version

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = u'2012, Radomir Dopieralski'
 # built documents.
 #
 # The short X.Y version.
-version = '1.5.8'
+version = '1.5.9'
 # The full version, including alpha/beta/rc tags.
 release = '1.5.8'
 
