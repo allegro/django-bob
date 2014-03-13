80c85b5df0170d5b59c8e482684383104520d442
Jakub Kędzierski <ext_jakub.kedzierski@allegro.pl>
2014-03-13 12:45:14 +0100

    Ajax update dependencies - not finished

--- bob/static/bootstrap/js/bob.js
+++ bob/static/bootstrap/js/bob.js
@@ -1,27 +1,105 @@
 /*jslint unparam: true */
 /*global jQuery: false */
-(function ($) {
+var djangoBob = function ($) {
     'use strict';
-    var bindDependencies;
+    var dependencyCondition,
+        bindAjaxUpdate,
+        bindDependencies,
+        setFieldValue;
+
+    dependencyCondition = function (dependencyValue, value) {
+        if ($.isArray(dependencyValue)) {
+            return dependencyValue.indexOf(value) !== -1;
+        } else if (dependencyValue === null) {
+            return true;
+        } else if (Object.prototype.toString.call(dependencyValue) &&
+                   Object.prototype.toString.call(dependencyValue)) {
+            return JSON.stringify(dependencyValue) === JSON.stringify(value);
+        }
+        return dependencyValue === value;
+    };
+
+    setFieldValue = function (field, value) {
+        var $field = $(field);
+        if (value == null) {
+            return;
+        }
+        if ($field.attr('type') == "checkbox") {
+            $field.prop("checked", value);
+        } else if ($.isArray(value) && $field.is('select')) {
+            $field.children().remove();
+            $.each(value, function(i, el) {
+                $field.append($('<option></option>').val(el[0]).html(el[1]))
+            });
+        } else {
+            $field.val(value);
+        }
+    };
+
+    bindAjaxUpdate = function (master, slave, value, url) {
+        var slavesName = "dependencySlaves";
+        var slaveDescription = {
+            field: slave,
+            value: value
+        };
+
+        if (typeof (master.data(slavesName) === "undefined")) {
+            master.data(slavesName, [slaveDescription]);
+            master.change(function () {
+                var passedSlaves = [],
+                    slavesNames = $(this).data(slavesName),
+                    slavesNamesLength = slavesNames.length,
+                    slaveObj;
+                for (var i = 0; i < slavesNamesLength; i++) {
+                    slaveObj = slavesNames[i];
+                    if (dependencyCondition(value, slaveObj.value)) {
+                        passedSlaves.push(slaveObj.field);
+                        slaveObj.field.addClass('value-loading');
+                    }
+                }
+
+                if (passedSlaves.length > 0) {
+                    $.ajax(url, {
+                        data: {
+                            value: value
+                        },
+                        dataType: "json",
+                        complete: function(request, status) {
+                            var slaveObj;
+                            for (var i = 0; i < slavesNamesLength; i++) {
+                                slaveObj = slavesNames[i];
+                                slaveObj.field.removeClass('value-loading');
+                            }
+                        },
+                        success: function(data, status, request) {
+                            var id,
+                                slaveObj;
+                            for (var i = 0; i < slavesNamesLength; i++) {
+                                slaveObj = slavesNames[i];
+                                id = slaveObj.field.attr('id').substr(3);
+                                if (typeof(data[id]) !== "undefined") {
+                                    setFieldValue(slaveObj.field, data[id]);
+                                }
+                            }
+                        },
+                        type: "POST"
+                    });
+                }
+            });
+        } else {
+            master.data(slavesName).push(slaveDescription);
+        }
+    };
 
     bindDependencies = function (form, dependencies) {
         $.each(dependencies, function (i, dep) {
-            var master, slave, slaveCtrl, condition;
+            var master, slave, slaveCtrl;
             master = $('#id_' + dep.master);
             slave = $('#id_' + dep.slave);
             slaveCtrl = slave.parents('.control-group');
-            if ($.isArray(dep.value)) {
-                condition = function (value) {
-                    return dep.value.indexOf(value) !== -1;
-                };
-            } else {
-                condition = function (value) {
-                    return dep.value === value;
-                };
-            }
             if (dep.action === "REQUIRE") {
                 master.change(function () {
-                    if (condition(master.val())) {
+                    if (dependencyCondition(master.val())) {
                         $(slaveCtrl).find('label').addClass('required');
                     } else {
                         $(slaveCtrl).find('label').removeClass('required');
@@ -29,7 +107,7 @@
                 });
             } else if (dep.action === "SHOW") {
                 master.change(function () {
-                    if (condition(master.val())) {
+                    if (dependencyCondition(master.val())) {
                         slave.removeAttr('disabled');
                         slaveCtrl.show();
                     } else {
@@ -37,44 +115,53 @@
                         slaveCtrl.hide();
                     }
                 });
+            } else if (dep.action === "AJAX_UPDATE") {
+                bindAjaxUpdate(master, slave, dep.value, dep.options.url);
             }
             master.change();
         });
     };
 
-    $(function ($) {
-        $('.bob-select-all').click(function () {
-            var table = $(this).closest('table');
-            table.find('input[name="select"]').prop('checked', true);
-            table.find('input[name="items"]').prop('checked', true);
-        });
-        $('.bob-select-none').click(function () {
-            var table = $(this).closest('table');
-            table.find('input[name="select"]').prop('checked', false);
-            table.find('input[name="items"]').prop('checked', false);
-            table.find('input[name="selectall"]').prop('checked', false);
-        });
-        $('.bob-select-toggle').click(function () {
-            var table = $(this).closest('table');
-            table.find('input[name="select"]').each(function () {
-                this.checked = !this.checked;
-            });
-            table.find('input[name="items"]').each(function () {
-                this.checked = !this.checked;
-            });
-            table.find('input[name="selectall"]').prop('checked', false);
-        });
-        $('.datepicker').datepicker({autoclose: true}).click(function () {
-            $('input.datepicker').not(this).datepicker('hide');
-        });
-        $.each($('form'), function (i, form) {
-            if (form.dataset.bobDependencies !== undefined) {
-                bindDependencies(
-                    form,
-                    $.parseJSON(form.dataset.bobDependencies)
-                );
-            }
-        });
-        $('.help-tooltip').tooltip();
+    return {
+        dependencyCondition: dependencyCondition,
+        bindAjaxUpdate: bindAjaxUpdate,
+        setFieldValue: setFieldValue,
+        bindDependencies: bindDependencies
+    }
+}(jQuery);
+
+$(document).ready(function () {
+    $('.bob-select-all').click(function () {
+        var table = $(this).closest('table');
+        table.find('input[name="select"]').prop('checked', true);
+        table.find('input[name="items"]').prop('checked', true);
     });
-}(jQuery));
+    $('.bob-select-none').click(function () {
+        var table = $(this).closest('table');
+        table.find('input[name="select"]').prop('checked', false);
+        table.find('input[name="items"]').prop('checked', false);
+        table.find('input[name="selectall"]').prop('checked', false);
+    });
+    $('.bob-select-toggle').click(function () {
+        var table = $(this).closest('table');
+        table.find('input[name="select"]').each(function () {
+            this.checked = !this.checked;
+        });
+        table.find('input[name="items"]').each(function () {
+            this.checked = !this.checked;
+        });
+        table.find('input[name="selectall"]').prop('checked', false);
+    });
+    $('.datepicker').datepicker({autoclose: true}).click(function () {
+        $('input.datepicker').not(this).datepicker('hide');
+    });
+    $.each($('form'), function (i, form) {
+        if (form.dataset.bobDependencies !== undefined) {
+            djangoBob.bindDependencies(
+                form,
+                $.parseJSON(form.dataset.bobDependencies)
+            );
+        }
+    });
+    $('.help-tooltip').tooltip();
+});
---
+++ bob/tests/js/DependenciesSpec.js
@@ -0,0 +1,65 @@
+describe("Dependencies", function() {
+
+  it("should be able to check if field value equals dependency value", function() {
+    expect(djangoBob.dependencyCondition('aaa', 'aaa')).toBeTruthy();
+    expect(djangoBob.dependencyCondition('a', 'b')).not.toBeTruthy();
+    expect(djangoBob.dependencyCondition('3', 3)).not.toBeTruthy();
+    expect(djangoBob.dependencyCondition({a: 'b'}, {a: 'b'})).toBeTruthy();
+    expect(djangoBob.dependencyCondition({a: 'b'}, "{a: 'b'}")).not.toBeTruthy();
+    expect(djangoBob.dependencyCondition(false, false)).toBeTruthy();
+  });
+
+  it("should always pass field value if dependency value was null", function() {
+    expect(djangoBob.dependencyCondition(null, 'aaa')).toBeTruthy();
+    expect(djangoBob.dependencyCondition(null, false)).toBeTruthy();
+    expect(djangoBob.dependencyCondition(null, null)).toBeTruthy();
+    expect(djangoBob.dependencyCondition(null, {a: 'b'})).toBeTruthy();
+  });
+
+  it("should match field value to element of dependency value array", function() {
+    expect(djangoBob.dependencyCondition([1, 2, 'aaa', 3], 'aaa')).toBeTruthy();
+    expect(djangoBob.dependencyCondition([1, 2, 'aaa', 3], false)).not.toBeTruthy();
+    expect(djangoBob.dependencyCondition([false, 3], false)).toBeTruthy();
+    expect(djangoBob.dependencyCondition([null], null)).toBeTruthy();
+    expect(djangoBob.dependencyCondition([null], {a: 'b'})).not.toBeTruthy();
+  });
+
+  it("should set value to text field", function () {
+    var field = $('<input type="text">');
+    djangoBob.setFieldValue(field, "Nobody expects spanish inquisition!");
+    expect(field.val()).toEqual("Nobody expects spanish inquisition!");
+    djangoBob.setFieldValue(field, "");
+    expect(field.val()).toEqual("");
+  });
+
+  it("shouldn't change field value if there is try to set null", function () {
+    var field = $('<input type="text">');
+    djangoBob.setFieldValue(field, "Nobody expects spanish inquisition!");
+    expect(field.val()).toEqual("Nobody expects spanish inquisition!");
+    djangoBob.setFieldValue(field, null);
+    expect(field.val()).toEqual("Nobody expects spanish inquisition!");
+  });
+
+  it("should set value to checkbox", function () {
+    var field = $('<input type="checkbox">');
+    djangoBob.setFieldValue(field, true);
+    expect(field.prop("checked")).toBeTruthy();
+    djangoBob.setFieldValue(field, false);
+    expect(field.prop("checked")).not.toBeTruthy();
+  });
+
+  it("should set value and options to selectbox", function () {
+    var field = $('<select></select>'),
+      values = [1, [
+        [1, 'single'], [2, 'widower'], [3, 'married']
+      ]],
+      options;
+    djangoBob.setFieldValue(field, values);
+    expect(field.children('[selected="selected"]').val()).toEqual(values[0]);
+    options = field.children().map(function (i, el) {
+      var $el= $(el);
+      return [[$el.val(), $el.html()]];
+    });
+    expect(options).toEqual(values);
+  });
+});
