6b0c430bd20d0d7a9485a8bf5cc948b4e614274d
Jakub Kędzierski <ext_jakub.kedzierski@allegro.pl>
2014-04-07 13:23:39 +0200

    Dependencies documentation

--- bob/forms/dependency.py
+++ bob/forms/dependency.py
@@ -1,5 +1,38 @@
 # -*- coding: utf-8 -*-
-"""All the objects needed to use the dependency mechanism"""
+"""All the objects needed to use the dependency mechanism.
+
+For dependency usage you need to:
+
+* add :class:`DependencyForm` as your form mixin,
+* (optional) define :class:`bob.views.dependency.DependencyView` if
+  :const:`AJAX_UPDATE` used.
+* define ``dependencies`` property in you form.
+
+``dependencies`` example:
+
+.. code-block:: python
+
+    @property
+    def dependencies(self):
+        return [
+            Dependency(
+                'slots',
+                'category',
+                dependency_conditions.MemberOf(
+                    AssetCategory.objects.filter(is_blade=True).all()
+                ),
+                SHOW,
+            ),
+            Dependency(
+                'location',
+                'owner',
+                dependency_conditions.NotEmpty(),
+                AJAX_UPDATE,
+                url=reverse('category_dependency_view'),
+                page_load_update=False,
+            )
+        ]
+"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -12,18 +45,29 @@ from bob.forms.dependency_conditions import DependencyCondition
 
 # dependencies types
 AJAX_UPDATE = 'AJAX_UPDATE'
+"""Load slave value by ajax request if master value met condition."""
 CLONE = 'CLONE'
+"""Copy master value to slave value."""
 SHOW = 'SHOW'
+"""Show slave field if master value met condition."""
 REQUIRE = 'REQUIRE'
+"""Set slave value required."""
 
 
 class Dependency(object):
-    """The single dependency: if master has value then do action on slave.
+    """The single dependency.
 
-    If action is ``AJAX_UPDATE`` you have to set `url` kwarg. Url corresponds
-    to
-    :py:class:bob.views.dependency.DependencyView
-    subclass.
+    If action is :const:`AJAX_UPDATE` you have to set ``url`` kwarg. Url
+    corresponds to :class:`bob.views.dependency.DependencyView` subclass.
+
+    :param slave: Dependency target field.
+    :param master: Dependency source field. It's value is condition param.
+    :param condition: :class:`.dependency_conditions.DependencyCondition`
+        subclass.
+    :param action: One of :const:`AJAX_UPDATE`, :const:`CLONE`,
+        :const:`SHOW`, :const:`REQUIRE`.
+    :param \*\*options: Another options, like ``url`` if action is
+        :const:`AJAX_UPDATE`.
     """
 
     def __init__(self, slave, master, condition, action, **options):
@@ -40,13 +84,20 @@ class Dependency(object):
             assert 'url' in options, "Source url not provided."
 
     def met(self, data):
-        """Return true if condition is met."""
+        """Return ``True`` if condition is met.
+
+        :param data: form.cleaned_data dictionary with master field value.
+        """
         val = data.get(self.master)
         return self.condition.met(val)
 
 
 class DependencyForm(object):  # Can't inherit Form due to metaclass conflict
-    """Form mixin with dependency feature."""
+    """Form mixin with dependency feature.
+
+    It should be used in django forms which includes dependencies.
+    It provides clean method with dependencies validation.
+    """
 
     dependencies = []
 
