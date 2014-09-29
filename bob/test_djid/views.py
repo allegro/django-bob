390c7e089cc02467aa30bcb57f5fd4dc72a12767
Szymon Pyżalski <szymon.pyzalski@allegro.pl>
2014-09-29 15:27:03 +0200

    Separated company into another model

--- bob/test_djid/views.py
+++ bob/test_djid/views.py
@@ -6,6 +6,7 @@ from bob.test_djid.models import Person
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 
+
 class PersonsGrid(Djid):
 
     class Meta:
