07b55a9bac81d081da6e3a67322814ef5e1ba0eb
Szymon Pyżalski <szymon.pyzalski@allegro.pl>
2014-10-21 13:34:10 +0200

    Mustache fixed

--- bob/djid/static/js/djid.js
+++ bob/djid/static/js/djid.js
@@ -11,11 +11,12 @@ define(['jquery', 'jqGrid', 'mustache'], function ($, jqGrid, Mustache) {
         )
     };
     function Djid(id, params) {
-        var new_params;
+        var new_params, that;
+        that = this;
         new_params = $.extend(
             {}, this.default_params, params
         )
-        this.id = '#' + id;
+        this.id = id;
         this.pager_id = '#' + id + '-pager';
         new_params['pager'] = this.pager_id;
         new_params['url'] = '/djid/' + id + '/',
@@ -35,15 +36,39 @@ define(['jquery', 'jqGrid', 'mustache'], function ($, jqGrid, Mustache) {
                 this.toggleToolbar();
             }
         });
-    }
+        this.jqgrid.navButtonAdd(this.pager_id, {
+            caption: '',
+            buttonicon: 'ui-icon-carat-1-e',
+            onClickButton: function () {
+                that.add_progress()
+            },
+            id: this.get_id('csv', true)
+            
+        });
+    };
     Djid.prototype.default_params = {
         datatype: "json",
         mtype: "GET",
         multiSort: true,
         scroll: true,
+    };
+    Djid.prototype.add_progress = function () {
+        progress = $('<div></div>').attr('id', this.get_id('progress', true));
+        $(this.get_id('pager_right')).append(progress);
+        progress.progressbar();
+        progress.progressbar('option', 'value', false);
+    };
+    Djid.prototype.get_id = function(suffix, drop_hash) {
+        var result;
+        result = this.id + '-' + suffix;
+        if (drop_hash) {
+            return result;
+        } else {
+            return '#' + result;
+        }
     }
     return {
         Djid: Djid
-    }
+    };
 });
 
